5f1e1fa1a9b366d4241ffb07caba6caa65922394
Ariel Santos Moraes <ariel.s.moraes@gmail.com>
2016-04-12 11:33:37 -0300

    Aula 10 - funções privadas (não usam module.exports) e componentes comuns

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,19 +1,16 @@
-var React = require("react");
+var React = require('react');
 var PropTypes = React.PropTypes;
 var styles = require('../styles');
 var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var UserDetails = require('./UserDetails');
+var MainContainer = require('./MainContainer');
 
-function puke (obj) {
-  return <pre>{JSON.stringify(obj, null, ' ')}</pre>
-}
-
-function ConfirmBattle(props){
+function ConfirmBattle (props) {
   return props.isLoading === true
     ? <p>LOADING</p>
-    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+    : <MainContainer>
         <h1>Confirm Players</h1>
         <div className='col-sm-8 col-sm-offset-2'>
           <UserDetailsWrapper header='Player 1'>
@@ -33,7 +30,7 @@ function ConfirmBattle(props){
             </Link>
           </div>
         </div>
-      </div>
+      </MainContainer>
 }
 
 ConfirmBattle.propTypes = {
